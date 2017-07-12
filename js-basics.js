dc3a26eb8d475bcf6eab3a390c9ef4684cb5397d
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-07-12 17:36:24 -0400

    first commit

---
+++ js-basics.js
@@ -0,0 +1,20 @@
+//Question 1
+var x=12
+var y=50
+function addNums(){
+	console.log(x + y);
+}
+addNums();
+
+var first="Wesley"
+var last="Stuckemeyer"
+
+function addName(){
+	console.log(first + " " + last);
+}
+
+addName();
+
+//Question 2
+var fruits=['grapes', 'apples', 'donuts', 'raisins'];
+var 
